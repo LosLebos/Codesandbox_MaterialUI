ad1e243d92a47bf0c7621c246d86022678c295dd
Bheeg <beneheeg@googlemail.com>
2023-03-21 14:40:49 +0100

    Initial commit

---
+++ src/Components/Sidebar.js
@@ -0,0 +1,8 @@
+import { Drawer } from "@mui/material";
+//import { List, ListItem } from "@mui/material/List";
+
+const Sidebar = () => {
+  return <Drawer>test</Drawer>;
+};
+
+export default Sidebar;
